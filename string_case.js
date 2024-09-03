83e64d1fef54fc54408ee1a2cc627f1e31dd88f9
Rownak Abdullah <rownakabdullahomi@gmail.com>
2024-09-03 19:27:39 +0600

    trim method done

--- string_case.js
+++ string_case.js
@@ -4,18 +4,25 @@ console.log(school);
 console.log(school.toLowerCase());
 console.log(school.toUpperCase());
 
-
 // uppercase: ABCD EFG
 // lowercase: abcd efg
 
-
 const subject = 'Chemistry';
 const book = 'ChemIsTry';
-
 
 if(subject.toLowerCase() === book.toLowerCase()){
     console.log('I am reading book.');
 }
 else {
     console.log('Not my book.');
+}
+
+const drink = '     water     ';
+const liquid = '  water     '
+
+if(drink.trim() === liquid.trim()){
+    console.log('Water is Life');
+}
+else {
+    console.log('Not Pure Water');
 }
